33bcffc1e9919fd3e9906bd3f3b5195df8d0d96e
Sanchit Gupta <sanchit.gupta@kohls.com>
2018-08-05 12:40:28 +0530

    E2E testing

--- electron/test/spec.js
+++ electron/test/spec.js
@@ -4,44 +4,46 @@ const electronPath = require('electron') // Require Electron from the binaries i
 const path = require('path')
 
 describe('Application launch', function () {
-    this.timeout(10000)
+    this.timeout(10000);
 
-    beforeEach(function () {
+    beforeEach(() => {
         this.app = new Application({
-            // Your electron path can be any binary
-            // i.e for OSX an example path could be '/Applications/MyApp.app/Contents/MacOS/MyApp'
-            // But for the sake of the example we fetch it from our node_modules.
             path: electronPath,
-
-            // Assuming you have the following directory structure
-
-            //  |__ my project
-            //     |__ ...
-            //     |__ main.js
-            //     |__ package.json
-            //     |__ index.html
-            //     |__ ...
-            //     |__ test
-            //        |__ spec.js  <- You are here! ~ Well you should be.
-
-            // The following line tells spectron to look and use the main.js file
-            // and the package.json located 1 level above.
             args: [path.join(__dirname, '..')]
         })
         return this.app.start()
     })
 
-    afterEach(function () {
+    afterEach(() => {
         if (this.app && this.app.isRunning()) {
             return this.app.stop()
         }
     })
 
-    it('shows an initial window', function () {
-        return this.app.client.getWindowCount().then(function (count) {
-            assert.equal(count, 1)
-            // Please note that getWindowCount() will return 2 if `dev tools` are opened.
-            // assert.equal(count, 2)
-        })
+    it('Test initial load of application', async () => {
+        const windowCount = await this.app.client.getWindowCount();
+        console.log(windowCount);
+        assert.equal(windowCount, 1);
+
+        const windowTitle = await this.app.client.getText('#mainTitle');
+        console.log(windowTitle);
+        assert.equal(windowTitle, 'CodeBook')
+
+        assert.ok(await this.app.client.isExisting('#mainIconSettings'))
     })
+
+    it('Test Settings Page', async () => {
+        assert.ok(await this.app.client.isExisting('#mainIconSettings'))
+        console.log('Setting Button exists')
+
+        // var settingButton = await this.app.client.selectByAttribute('id', '#mainIconSettings')
+        // console.log(settingButton)
+        // settingButton.click()
+
+        // var moreSettingButton = await this.app.client.selectByAttribute('ng-reflect-router-link', 'settings')
+        // console.log(moreSettingButton)
+        // moreSettingButton.click()
+
+    })
+
 })
